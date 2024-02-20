8af1f3b22045bebca007fae78017efffdcf46bb3
prakash <pr0018908@gmail.com>
2024-02-20 16:32:39 +0530

    main

---
+++ src/main/java/com/pp/SpringRestApiTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.pp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiTicketBookingApplication.class, args);
+	}
+
+}
