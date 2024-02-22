2eb154b02ad627f4753380d266bab67967294450
prakash <pr0018908@gmail.com>
2024-02-23 00:00:17 +0530

    given annotations for the documention of  the api

--- src/main/java/com/pp/SpringRestApiTicketBookingApplication.java
+++ src/main/java/com/pp/SpringRestApiTicketBookingApplication.java
@@ -3,7 +3,16 @@ package com.pp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Ticket Booking API", 
+                  version = "1.0",
+                  description = "AN API to Book a Ticket"),
+                  servers = @Server(url = "http://localhost:8787/TicketBookingApp",
+          				            description = " This API URL will help to book ticket"))
 public class SpringRestApiTicketBookingApplication {
 
 	public static void main(String[] args) {
